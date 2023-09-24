5afe872656e10e775945420bb1247578cb252155
Braulio Zapata <brauliozapata@destiny.ws>
2023-09-24 18:42:27 -0400

    feature: Add unix test and factory

---
+++ database/factories/CandidateFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Candidate>
+ */
+class CandidateFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'owner' => 1,
+            'created_by' => 1,
+            'source' => Str::random(10),
+        ];
+    }
+}
